79e0e17bb7f6920dc37bd7f008eed73b34020e62
shre4ash <shreyashkumbhar9@outlook.com>
2026-09-11 21:03:29 +0530

    global chaches in middelwares

---
+++ Class-3.1/index1.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.post("/health-checkup" , (req, res) => {
+
+    const kidneys = req.body.kidneys;
+    const kidneyLength = kidneys.length;
+
+    res.send("you have " + kidneyLength + " kidneys");
+});
+
+app.use((err, req, res, next) => {
+    res.json({
+        msg: "Sorry something is up with our server"
+    })
+})
+
+app.listen(3001);
